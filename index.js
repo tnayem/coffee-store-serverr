5f686c4717b80168439a65b7d2cccd3861efc545
tnayem <tasdidnayem@gmail.com>
2025-10-18 20:50:30 +0600

    using post method

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = 3000;
 
 // middleware
 app.use(cors())
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.etjzxzd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri)
@@ -15,6 +16,13 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const coffeesCollection = client.db('coffeeDb').collection('coffees')
+        // Post coffee data from clint side
+        app.post('/coffees',async(req,res)=>{
+            const coffee = req.body;
+            const result = await coffeesCollection.insertOne(coffee)
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
