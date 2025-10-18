9d666747c88b08b4af0f4272a1a677dad44f45aa
tnayem <tasdidnayem@gmail.com>
2025-10-18 08:02:30 +0600

    connect to mongodb

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require('cors')
@@ -5,7 +6,25 @@ const port = 3000;
 
 
 // middleware
+app.use(cors())
 
+// user: coffee_store_server
+// password: jWwTdTjOeO4dys2J
+const uri = "mongodb+srv://coffee_store_server:jWwTdTjOeO4dys2J@cluster0.etjzxzd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri)
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
